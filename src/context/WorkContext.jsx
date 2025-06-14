79dc17c18a585f47578824c1ade4824bae3c5e57
Anurag-git04 <shawanurag18@gmail.com>
2025-06-14 12:26:26 +0530

    new context

--- src/context/WorkContext.jsx
+++ src/context/WorkContext.jsx
@@ -1,5 +1,51 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const WorkContext = createContext()
+const WorkContext = createContext();
 
-export default WorkContext;
+const WorkProviderComponent = ({ children }) => {
+  const [log, setLogged] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch('https://workasana-backend-ecru.vercel.app/project');
+      const data = await res.json();
+      console.log(data);
+      return data.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const fetchTaskData = async () => {
+    try {
+      const res = await fetch('https://workasana-backend-ecru.vercel.app/tasks');
+      const data = await res.json();
+      console.log(data);
+      return data.tasks;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const fetchTeam = async () => {
+    try {
+      const res = await fetch('https://workasana-backend-ecru.vercel.app/teams');
+      const data = await res.json();
+      console.log(data);
+      return data.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  return (
+    <WorkContext.Provider value={{ setLogged, log, fetchData, fetchTaskData, fetchTeam }}>
+      {children}
+    </WorkContext.Provider>
+  );
+};
+
+export { WorkContext, WorkProviderComponent };
